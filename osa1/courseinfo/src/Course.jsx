ce9f97d8628bfa0a8f0f667f713fd5582e524217
Alona Podolian <alona.podolian@edu.keuda.fi>
2026-07-18 21:02:59 +0300

    Exercises 2.1-2.5

---
+++ osa1/courseinfo/src/Course.jsx
@@ -0,0 +1,40 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+     {props.parts.map(part => 
+      <Part key={part.id} part={part} />
+     )}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  const total = props.parts.reduce(
+  (sum, part) => sum + part.exercises, 0
+ )
+ return <strong>total of {total} exercises</strong>
+}
+
+const Course = (props) => {
+  return (
+   <div>
+    <Header course={props.course.name} />
+    <Content parts={props.course.parts} />
+    <Total parts={props.course.parts} />
+   </div>
+  )
+}
+
+export default Course
